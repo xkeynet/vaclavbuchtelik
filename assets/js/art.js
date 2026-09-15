01e9a5eee8a2fa222f64c55409239450fa1f314a
XKEYNET <xkeynet@gmail.com>
2026-09-15 08:39:33 +0200

    Add titles and details to ART array
    Updated the ART array to include titles and details for each artwork.

--- assets/js/art.js
+++ assets/js/art.js
@@ -1,58 +1,369 @@
 const ART_CDN = 'https://cdn.tikboo.com/art';
 
 const ART = [
-  { id: 1, src: `${ART_CDN}/1.jpeg` },
-  { id: 2, src: `${ART_CDN}/2.jpeg` },
-  { id: 3, src: `${ART_CDN}/3.jpeg` },
-  { id: 4, src: `${ART_CDN}/4.jpeg` },
-  { id: 5, src: `${ART_CDN}/5.jpeg` },
-  { id: 6, src: `${ART_CDN}/6.webp` },
-  { id: 7, src: `${ART_CDN}/7.webp` },
-  { id: 8, src: `${ART_CDN}/8.webp` },
-  { id: 9, src: `${ART_CDN}/9.webp` },
-  { id: 10, src: `${ART_CDN}/10.webp` },
-  { id: 11, src: `${ART_CDN}/11.webp` },
-  { id: 12, src: `${ART_CDN}/12.webp` },
-  { id: 13, src: `${ART_CDN}/13.webp` },
-  { id: 14, src: `${ART_CDN}/14.webp` },
-  { id: 15, src: `${ART_CDN}/15.webp` },
-  { id: 16, src: `${ART_CDN}/16.webp` },
-  { id: 17, src: `${ART_CDN}/17.webp` },
-  { id: 18, src: `${ART_CDN}/18.webp` },
-  { id: 19, src: `${ART_CDN}/19.webp` },
-  { id: 20, src: `${ART_CDN}/20.webp` },
-  { id: 21, src: `${ART_CDN}/21.webp` },
-  { id: 22, src: `${ART_CDN}/22.webp` },
-  { id: 23, src: `${ART_CDN}/23.webp` },
-  { id: 24, src: `${ART_CDN}/24.webp` },
-  { id: 25, src: `${ART_CDN}/25.webp` },
-  { id: 26, src: `${ART_CDN}/26.webp` },
-  { id: 27, src: `${ART_CDN}/27.webp` },
-  { id: 28, src: `${ART_CDN}/28.webp` },
-  { id: 29, src: `${ART_CDN}/29.webp` },
-  { id: 30, src: `${ART_CDN}/30.webp` },
-  { id: 31, src: `${ART_CDN}/31.webp` },
-  { id: 32, src: `${ART_CDN}/32.webp` },
-  { id: 33, src: `${ART_CDN}/33.webp` },
-  { id: 34, src: `${ART_CDN}/34.webp` },
-  { id: 35, src: `${ART_CDN}/35.webp` },
-  { id: 36, src: `${ART_CDN}/36.webp` },
-  { id: 37, src: `${ART_CDN}/37.webp` },
-  { id: 38, src: `${ART_CDN}/38.webp` },
-  { id: 39, src: `${ART_CDN}/39.webp` },
-  { id: 40, src: `${ART_CDN}/40.webp` },
-  { id: 41, src: `${ART_CDN}/41.webp` },
-  { id: 42, src: `${ART_CDN}/42.webp` },
-  { id: 43, src: `${ART_CDN}/43.webp` },
-  { id: 44, src: `${ART_CDN}/44.webp` },
-  { id: 45, src: `${ART_CDN}/45.webp` },
-  { id: 46, src: `${ART_CDN}/46.webp` },
-  { id: 47, src: `${ART_CDN}/47.jpg` },
-  { id: 48, src: `${ART_CDN}/48.webp` },
-  { id: 49, src: `${ART_CDN}/49.webp` },
-  { id: 50, src: `${ART_CDN}/50.jpg` },
-  { id: 51, src: `${ART_CDN}/51.jpg` },
-  { id: 52, src: `${ART_CDN}/52.jpg` }
+  {
+    id: 1,
+    src: `${ART_CDN}/1.jpeg`,
+    title: 'See-Saw II',
+    details: '120x90 cm, acrylic and oil on canvas, 2026'
+  },
+
+  {
+    id: 2,
+    src: `${ART_CDN}/2.jpeg`,
+    title: 'See-Saw I',
+    details: '120x90 cm, acrylic and oil on canvas, 2026'
+  },
+
+  {
+    id: 3,
+    src: `${ART_CDN}/3.jpeg`,
+    title: 'Unthroned',
+    details: '150x120, oil on canvas, 2026'
+  },
+
+  {
+    id: 4,
+    src: `${ART_CDN}/4.jpeg`,
+    title: 'The Game',
+    details: '40x30 cm, oil on canvas, 2026'
+  },
+
+  {
+    id: 5,
+    src: `${ART_CDN}/5.jpeg`,
+    title: 'Life in Dissonance',
+    details: '250x200 cm, acrylic, oil on canvas, 2025'
+  },
+
+  {
+    id: 6,
+    src: `${ART_CDN}/6.webp`,
+    title: 'Prophet',
+    details: '50x40, acrylic and oil on canvas, 2026'
+  },
+
+  {
+    id: 7,
+    src: `${ART_CDN}/7.webp`,
+    title: 'You Are Garinus II',
+    details: '68x48 cm, watercolour, 2025'
+  },
+
+  {
+    id: 8,
+    src: `${ART_CDN}/8.webp`,
+    title: 'Tool',
+    details: '80x60 cm, oil on canvas, 2025'
+  },
+
+  {
+    id: 9,
+    src: `${ART_CDN}/9.webp`,
+    title: 'Burning Skull (After Kubišta)',
+    details: '40x30 cm, acrylic, oil, nails on wood 2024'
+  },
+
+  {
+    id: 10,
+    src: `${ART_CDN}/10.webp`,
+    title: 'My heart works too much',
+    details: '140x120 cm, oil on canvas, 2025'
+  },
+
+  {
+    id: 11,
+    src: `${ART_CDN}/11.webp`,
+    title: 'Meta',
+    details: '50x40 cm, acrylic, oil on canvas, 2024'
+  },
+
+  {
+    id: 12,
+    src: `${ART_CDN}/12.webp`,
+    title: 'MBB',
+    details: '40x30 cm, acrylic and oil canvas, 2024'
+  },
+
+  {
+    id: 13,
+    src: `${ART_CDN}/13.webp`,
+    title: 'Verdict',
+    details: '40x30 cm, oil on canvas, 2024'
+  },
+
+  {
+    id: 14,
+    src: `${ART_CDN}/14.webp`,
+    title: 'Tatters',
+    details: '50x40 cm, oil on canvas, 2024'
+  },
+
+  {
+    id: 15,
+    src: `${ART_CDN}/15.webp`,
+    title: 'Then',
+    details: '40x30 cm, acrylic and oil on canvas, 2024'
+  },
+
+  {
+    id: 16,
+    src: `${ART_CDN}/16.webp`,
+    title: 'Belladonna',
+    details: '40x30 cm, acrylic and oil on canvas, 2024'
+  },
+
+  {
+    id: 17,
+    src: `${ART_CDN}/17.webp`,
+    title: 'Thorns hurt less in the light',
+    details: '40x30 cm, oil on canvas, 2024'
+  },
+
+  {
+    id: 18,
+    src: `${ART_CDN}/18.webp`,
+    title: 'Done!',
+    details: '50x40 cm, oil on canvas, 2024'
+  },
+
+  {
+    id: 19,
+    src: `${ART_CDN}/19.webp`,
+    title: 'Over the Edge',
+    details: '80x60 cm, acrylic and oil on canvas, 2024'
+  },
+
+  {
+    id: 20,
+    src: `${ART_CDN}/20.webp`,
+    title: 'Its a great pleasure to be a part of group show BREAD 31.1. 24 - 17.2. 24',
+    details: ''
+  },
+
+  {
+    id: 21,
+    src: `${ART_CDN}/21.webp`,
+    title: 'Its a great pleasure to be a part of group show BREAD 31.1. 24 - 17.2. 24 (2)',
+    details: ''
+  },
+
+  {
+    id: 22,
+    src: `${ART_CDN}/22.webp`,
+    title: 'Mood',
+    details: '40x30 cm, oil on canvas, 2024'
+  },
+
+  {
+    id: 23,
+    src: `${ART_CDN}/23.webp`,
+    title: 'Brian Teaser',
+    details: '50x40 cm, oil on canvas, 2023'
+  },
+
+  {
+    id: 24,
+    src: `${ART_CDN}/24.webp`,
+    title: 'Allegory II',
+    details: '160x120 cm, oil on canvas, 2023'
+  },
+
+  {
+    id: 25,
+    src: `${ART_CDN}/25.webp`,
+    title: 'At the Dining table',
+    details: '50x40 cm, oil on canvas, 2023'
+  },
+
+  {
+    id: 26,
+    src: `${ART_CDN}/26.webp`,
+    title: 'The Hour',
+    details: '160x120 cm, acrylic and oil on canvas, 2023'
+  },
+
+  {
+    id: 27,
+    src: `${ART_CDN}/27.webp`,
+    title: 'The Edge',
+    details: '40x30 cm, acrylic and oil on canvas, 2023'
+  },
+
+  {
+    id: 28,
+    src: `${ART_CDN}/28.webp`,
+    title: 'Theme Digger',
+    details: '40x30 cm, acrylic and oil on canvas, 2022'
+  },
+
+  {
+    id: 29,
+    src: `${ART_CDN}/29.webp`,
+    title: 'The Licker',
+    details: '50x40 cm, oil on canvas, 2022'
+  },
+
+  {
+    id: 30,
+    src: `${ART_CDN}/30.webp`,
+    title: 'Creeping',
+    details: '40x30 cm, oil on canvas, 2022'
+  },
+
+  {
+    id: 31,
+    src: `${ART_CDN}/31.webp`,
+    title: 'Creeping',
+    details: '50x40 cm, oil on canvas, 2022'
+  },
+
+  {
+    id: 32,
+    src: `${ART_CDN}/32.webp`,
+    title: 'Banderola',
+    details: '40x30 cm, oil on canvas, 2022'
+  },
+
+  {
+    id: 33,
+    src: `${ART_CDN}/33.webp`,
+    title: 'Drill biter',
+    details: '220x160 cm, oil on canvas, 2022'
+  },
+
+  {
+    id: 34,
+    src: `${ART_CDN}/34.webp`,
+    title: 'Helix',
+    details: '40x30 cm, oil on canvas, 2022'
+  },
+
+  {
+    id: 35,
+    src: `${ART_CDN}/35.webp`,
+    title: 'The Tamer',
+    details: '200x150 cm, oil on canvas, 2022'
+  },
+
+  {
+    id: 36,
+    src: `${ART_CDN}/36.webp`,
+    title: 'Discourse',
+    details: '40x30 cm, oil on canvas, 2022'
+  },
+
+  {
+    id: 37,
+    src: `${ART_CDN}/37.webp`,
+    title: 'Origami',
+    details: '50x40 cm, oil on canvas, 2022'
+  },
+
+  {
+    id: 38,
+    src: `${ART_CDN}/38.webp`,
+    title: 'Reptile symbol',
+    details: '50x40 cm, oil on canvas, 2022'
+  },
+
+  {
+    id: 39,
+    src: `${ART_CDN}/39.webp`,
+    title: 'A moment after',
+    details: '250x200 cm, oil on canvas, 2022'
+  },
+
+  {
+    id: 40,
+    src: `${ART_CDN}/40.webp`,
+    title: 'Soft Prong',
+    details: '120x70 cm, oil on canvas, 2022'
+  },
+
+  {
+    id: 41,
+    src: `${ART_CDN}/41.webp`,
+    title: 'Painters topic',
+    details: '30x20 cm, oil on canvas, 2022'
+  },
+
+  {
+    id: 42,
+    src: `${ART_CDN}/42.webp`,
+    title: 'Sign builder',
+    details: '40x30 cm, oil on canvas, 2022'
+  },
+
+  {
+    id: 43,
+    src: `${ART_CDN}/43.webp`,
+    title: 'Painters',
+    details: '40x30 cm, oil on canvas, 2022'
+  },
+
+  {
+    id: 44,
+    src: `${ART_CDN}/44.webp`,
+    title: 'Sign of Evil',
+    details: '30x20 cm, oil on canvas, 2022'
+  },
+
+  {
+    id: 45,
+    src: `${ART_CDN}/45.webp`,
+    title: 'Two signs',
+    details: '40x30 cm, oil on canvas, 2022'
+  },
+
+  {
+    id: 46,
+    src: `${ART_CDN}/46.webp`,
+    title: 'Family sign',
+    details: '40x30 cm, oil on canvas, 2022'
+  },
+
+  {
+    id: 47,
+    src: `${ART_CDN}/47.jpg`,
+    title: 'KVAN',
+    details: '200x150 cm, oil on canvas, 2021'
+  },
+
+  {
+    id: 48,
+    src: `${ART_CDN}/48.webp`,
+    title: 'Headless painter',
+    details: '40x30 cm, oil on canvas, 2021'
+  },
+
+  {
+    id: 49,
+    src: `${ART_CDN}/49.webp`,
+    title: 'Headless painter',
+    details: '40x30 cm, oil on canvas, 2021'
+  },
+
+  {
+    id: 50,
+    src: `${ART_CDN}/50.jpg`,
+    title: 'Chemistry',
+    details: '26x20 cm, collage, 2019'
+  },
+
+  {
+    id: 51,
+    src: `${ART_CDN}/51.jpg`,
+    title: 'Bathing II',
+    details: '25x19 cm, collage, 2019'
+  },
+
+  {
+    id: 52,
+    src: `${ART_CDN}/52.jpg`,
+    title: 'Sněm V',
+    details: 'cca 25x20 cm, koláž, 2018'
+  }
 ];
 
 window.ART = ART;
